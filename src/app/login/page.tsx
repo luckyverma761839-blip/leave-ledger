343a1626e84c44ee787dcca25e21895193a63a3f
luckyverma761839-blip <luckyverma761839@gmail.com>
2026-07-10 17:11:15 +0530

    feat: add login page UI

---
+++ src/app/login/page.tsx
@@ -0,0 +1,60 @@
+export default function LoginPage() {
+  return (
+    <main className="min-h-screen bg-slate-950 flex items-center justify-center px-6">
+
+      <div className="w-full max-w-md bg-slate-900 border border-slate-800 rounded-2xl p-8">
+
+        <h1 className="text-4xl font-bold text-white text-center">
+          Welcome Back 👋
+        </h1>
+
+        <p className="text-gray-400 text-center mt-3">
+          Login to your LeaveLedger account
+        </p>
+
+        <form className="mt-8 space-y-5">
+
+          <div>
+            <label className="block text-gray-300 mb-2">
+              Email
+            </label>
+
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-full bg-slate-800 border border-slate-700 rounded-lg px-4 py-3 text-white outline-none focus:border-blue-500"
+            />
+          </div>
+
+          <div>
+            <label className="block text-gray-300 mb-2">
+              Password
+            </label>
+
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="w-full bg-slate-800 border border-slate-700 rounded-lg px-4 py-3 text-white outline-none focus:border-blue-500"
+            />
+          </div>
+
+          <button
+            className="w-full bg-blue-600 hover:bg-blue-700 py-3 rounded-lg font-semibold transition"
+          >
+            Login
+          </button>
+
+        </form>
+
+        <p className="text-center text-gray-400 mt-6">
+          Don't have an account?
+          <span className="text-blue-400 cursor-pointer">
+            {" "}Sign Up
+          </span>
+        </p>
+
+      </div>
+
+    </main>
+  );
+}
